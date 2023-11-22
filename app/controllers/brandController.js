5e4118c649f9c0c7584ce2bfa6121cc3cce61d50
kyotakuu <chauvelon.theo@gmail.com>
2023-11-22 13:52:33 +0100

    step 3 finit

--- app/controllers/brandController.js
+++ app/controllers/brandController.js
@@ -1,18 +1,29 @@
 const { Brand } = require("../models");
 
 const brandController = {
+  showBrandsPage: async (req, res) => {
+    try {
+      const brandsList = await Brand.findAll();
 
-    showBrandsPage: async (req, res) => {
-        try {
-            const brandsList = await Brand.findAll();
-
-            res.render('brands', { brandsList });
-        } catch (error) {
-            console.log(error);
-            res.status(500).send(error);
-        }
-        
+      res.render("brands", { brandsList });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
     }
-}
+  },
+
+  showProductsByBrand: async (req, res) => {
+    try {
+      const targetId = req.params.id;
+
+      const brand = await Brand.findByPk(targetId, { include: "products" });
+
+      res.render("brand", { brand });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+  },
+};
 
 module.exports = brandController;
--- app/router.js
+++ app/router.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const controller = require('./controllers/mainController');
 const brandController = require('./controllers/brandController');
+const controller = require('./controllers/mainController');
 
 const router = express.Router();
 
 router.get('/', controller.home);
 
 router.get('/product/:id', controller.product);
-router.get('/brands',brandController.showBrandsPage)
+
+router.get('/brands', brandController.showBrandsPage);
+
+router.get('/brands/:id', brandController.showProductsByBrand);
 
 module.exports = router;
